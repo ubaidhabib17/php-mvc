dd814eda56ddd97fca49e30c443da248f94c568e
ubaidhabib17 <ubaidhabib17@gmail.com>
2020-11-07 11:08:42 +0700

    routing

--- app/core/App.php
+++ app/core/App.php
@@ -4,6 +4,17 @@ class App {
 
     public function __construct()
     {
-        echo "Halo ges";
+        $url = $this->parseUrl();
+        var_dump($url);
+    }
+
+    public function parseUrl()
+    {
+        if (isset($_GET['url'])) {
+            $url = rtrim($_GET['url'], '/');
+            $url =  filter_var($url, FILTER_SANITIZE_URL);
+            $url = explode('/', $url);
+            return $url;
+        }
     }
 }
